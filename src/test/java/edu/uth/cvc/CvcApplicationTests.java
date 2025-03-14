88783f8907d0e1cb8b8f64977a7741ab454840fc
VOLEHUYDAT <huydat13825@gmail.com>
2025-03-14 21:47:17 +0700

    first page

---
+++ src/test/java/edu/uth/cvc/CvcApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.uth.cvc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
