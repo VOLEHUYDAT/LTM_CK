e5199033cbf2df477bc08cdad21646cdfaeefbc6
VOLEHUYDAT <huydat13825@gmail.com>
2025-08-22 22:03:04 +0700

    initial

---
+++ src/main/java/edu/uth/cvc/controllers/HomeController.java
@@ -0,0 +1,11 @@
+package edu.uth.cvc.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+@Controller
+public class HomeController{
+    @GetMapping ("/home")
+    public String HomePage(){
+        return "home";
+    }
+}
